35d3fbd3b7dcc4e6c44c6370e1b52b7f18f1d228
Tima Vysotski <vysotski.tima@gmail.com>
2019-04-14 20:08:45 +0300

    JSON weather call in CurrentWeatherViewController

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -16,9 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if let rootVC = window?.rootViewController as? CurrnetWeatherViewController {
-            rootVC.container = persistentContainer
-        }
+        // Override point for customization after application launch.
         return true
     }
 
--- WeatherApp/Controllers/CurrentWeatherViewController.swift
+++ WeatherApp/Controllers/CurrentWeatherViewController.swift
@@ -1,22 +1,68 @@
 import UIKit
 import CoreData
+import Alamofire
+import SwiftyJSON
+import NVActivityIndicatorView
+import CoreLocation
 
 
-class CurrnetWeatherViewController : UIViewController {
+class CurrnetWeatherViewController : UIViewController, CLLocationManagerDelegate {
    
     @IBOutlet weak var navigationBar: UINavigationItem!
     
-    @IBOutlet weak var cityTextLabel: UILabel!
-    @IBOutlet weak var temperatureTextLabel: UILabel!
-    @IBOutlet weak var temperatureScaleTextLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var conditionLabal: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var iconNameLabel: UILabel!
     
-    var container: NSPersistentContainer!
+    var activityIndicator: NVActivityIndicatorView!
+    var cityName = "Minsk"
+    
+    let locationManager = CLLocationManager()
+    let apiKey = "03f07442bb1ee385c3c61ec678db4d9b"
+    // https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&appid=03f07442bb1ee385c3c61ec678db4d9b
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpBackGroundView()
         escapeNavigationBar()
         
+        let indicatorSize: CGFloat = 70
+        let indicatorFrame = CGRect(x: (view.frame.width - indicatorSize/2), y: (view.frame.height - indicatorSize/2), width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
+        activityIndicator.backgroundColor = UIColor.black
+        view.addSubview(activityIndicator)
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        activityIndicator.startAnimating()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    Alamofire.request("https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&units=metric&appid=\(apiKey)").responseJSON {
+        
+        response in
+        self.activityIndicator.stopAnimating()
+        if let responseStr = response.result.value{
+            let jsonResponse = JSON(responseStr)
+            let jsonWeather = jsonResponse["weather"].array![0]
+            let jsonTemp = jsonResponse["main"]
+            let iconName = jsonWeather["icon"].stringValue
+            
+            self.locationLabel.text = jsonResponse["name"].stringValue
+            self.conditionImageView.image = UIImage(named: "01d")
+            self.conditionLabal.text = jsonWeather["description"].stringValue
+            self.temperatureLabel.text = "\(Int(round(jsonTemp["temp"].doubleValue)))°C"
+        }
+        }
     }
     
 }
@@ -25,5 +71,11 @@ extension UIViewController {
     func setUpBackGroundView(){
         let backGroundImage : UIImage =  UIImage(image: .lightBackground)
         return self.view.layer.contents = backGroundImage.cgImage
+    }
+}
+
+extension CurrnetWeatherViewController {
+    func startingLocationManager(){
+        
     }
 }
